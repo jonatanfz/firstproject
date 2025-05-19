2cb5041cbc162056f22f35e01a730e9851c848aa
Jonatan Fiuza <jonatan.fz@hotmail.com>
2025-05-19 14:13:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let botao = document.querySelector("#botao");
+//botao.style.background="blue";
+
+botao.addEventListener("click", e =>{document.getElementById("mensagem").textContent = "VAI CORINTHIANS!!!";});
+
+
